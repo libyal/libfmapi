bd46eb958fa0ecfa1e3d8af5ad667783ce57b9c4
Joachim Metz <joachim.metz@gmail.com>
2017-01-12 09:15:29 +0100

    Worked on tests and applied updates

--- libfmapi/libfmapi_x400_object_identifier.h
+++ libfmapi/libfmapi_x400_object_identifier.h
@@ -1,7 +1,7 @@
 /*
  * X.400 object identifier (MAPIOID) functions
  *
- * Copyright (C) 2009-2016, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2009-2017, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
--- tests/fmapi_test_one_off_entry_identifier.c
+++ tests/fmapi_test_one_off_entry_identifier.c
@@ -1,7 +1,7 @@
 /*
- * Library one_off_entry_identifier type testing program
+ * Library one_off_entry_identifier type test program
  *
- * Copyright (C) 2009-2016, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2009-2017, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
