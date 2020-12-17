9075a8af0154556b90edcf27d9d44ca01ff1a225
Joachim Metz <joachim.metz@gmail.com>
2020-12-17 06:54:39 +0100

    Applied updates

--- libfmapi/libfmapi_extern.h
+++ libfmapi/libfmapi_extern.h
@@ -30,7 +30,7 @@
 
 #include <libfmapi/extern.h>
 
-#if defined( __CYGWIN__ )
+#if defined( __CYGWIN__ ) || defined( __MINGW32__ )
 #define LIBFMAPI_EXTERN_VARIABLE	extern
 #else
 #define LIBFMAPI_EXTERN_VARIABLE	LIBFMAPI_EXTERN
