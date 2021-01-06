6f3b19ffd18d2bc5454a3b0be90e0cd19c0ebde0
Joachim Metz <joachim.metz@gmail.com>
2021-01-06 09:28:30 +0100

    Applied updates

--- tests/fmapi_test_one_off_entry_identifier.c
+++ tests/fmapi_test_one_off_entry_identifier.c
@@ -1,7 +1,7 @@
 /*
  * Library one_off_entry_identifier type test program
  *
- * Copyright (C) 2009-2020, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2009-2021, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
